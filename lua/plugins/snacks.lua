89a21cda91479ff448d370c89c1c491b35142312
Jonatan Westling <jonatan@vestling.se>
2025-05-18 10:40:30 +0200

    Added new start page

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        sections = {
+          {
+            section = "terminal",
+            cmd = "chafa /Users/jonatanwestling/.config/nvim/creeper.png --format symbols --symbols vhalf --size 30x17 --stretch; sleep .1",
+            --cmd = "chafa Documents/jonis/wallpapers/mc.png --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1",
+            --cmd = "chafa Documents/jonis/wallpapers/chicken.png --format symbols --symbols vhalf --size 120x17 --stretch; sleep .1",
+            height = 17,
+            padding = 1,
+            indent = 15,
+          },
+          {
+            pane = 2,
+            { section = "keys", gap = 1, padding = 1 },
+            { section = "startup" },
+          },
+        },
+      },
+    },
+  },
+}
