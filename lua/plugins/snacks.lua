4e4e60ae8660679103bca60e3bc1b08f097962fb
Jonatan Westling <jonatanwestling4@gmail.com>
2025-10-22 21:24:08 +0200

    Feat: use pokemon-colorscripts instead of image

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,8 +6,8 @@ return {
         sections = {
           {
             section = "terminal",
-            cmd = "chafa /Users/jonatanwestling/.config/nvim/creeper.png --format symbols --symbols vhalf --size 30x17 --stretch; sleep .1",
-            --cmd = "chafa Documents/jonis/wallpapers/mc.png --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1",
+            -- cmd = "chafa /Users/jonatanwestling/.config/nvim/creeper.png --format symbols --symbols vhalf --size 30x17 --stretch; sleep .1",
+            cmd = "pokemon-colorscripts -n gengar --no-title; sleep .1",
             --cmd = "chafa Documents/jonis/wallpapers/chicken.png --format symbols --symbols vhalf --size 120x17 --stretch; sleep .1",
             height = 17,
             padding = 1,
