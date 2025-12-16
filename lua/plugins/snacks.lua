200deaa3a17fd9fc3af9b5c8e2648b5accc07ddb
jonatan westling <jonatanwestling4@gmail.com>
2025-12-16 22:14:26 +0100

    fixed path issues for pokemon-colorscript

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,16 +6,22 @@ return {
         sections = {
           {
             section = "terminal",
-            -- cmd = "chafa /Users/jonatanwestling/.config/nvim/creeper.png --format symbols --symbols vhalf --size 30x17 --stretch; sleep .1",
-            cmd = "pokemon-colorscripts -n gengar --no-title; sleep .1",
-            --cmd = "chafa Documents/jonis/wallpapers/chicken.png --format symbols --symbols vhalf --size 120x17 --stretch; sleep .1",
+            cmd = (function()
+              local poke = vim.fn.exepath("pokemon-colorscripts")
+              if poke == "" then
+                -- fallback command:
+                return "echo 'pokemon-colorscripts not found'; sleep 1"
+              else
+                return poke .. " -r --no-title; sleep .1"
+              end
+            end)(),
             height = 17,
             padding = 1,
             indent = 15,
           },
           {
             pane = 2,
-            { section = "keys", gap = 1, padding = 1 },
+            { section = "keys",   gap = 1, padding = 1 },
             { section = "startup" },
           },
         },
