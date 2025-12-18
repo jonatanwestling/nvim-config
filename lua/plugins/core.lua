1c9ee84286c06b9395ca0c7c4f4e64d0bdee10f5
Jonatan Westling <jonatanwestling4@gmail.com>
2025-12-18 19:06:35 +0100

    update: theme

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,13 +1,46 @@
+-- return {
+--   "projekt0n/github-nvim-theme",
+--   name = "github-theme",
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     require("github-theme").setup({
+--       -- ...
+--     })
+--
+--     vim.cmd("colorscheme github_dark_dimmed")
+--   end,
+-- }
+
+-- return {
+--   "rose-pine/neovim",
+--   name = "rose-pine",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("rose-pine").setup({
+--       variant = "moon", -- main | moon | dawn
+--       dark_variant = "moon",
+--       styles = {
+--         transparency = false,
+--       },
+--     })
+--
+--     vim.cmd.colorscheme("rose-pine")
+--   end,
+-- }
+
 return {
-  "projekt0n/github-nvim-theme",
-  name = "github-theme",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+  "ellisonleao/gruvbox.nvim",
+  lazy = false,
+  priority = 1000,
   config = function()
-    require("github-theme").setup({
-      -- ...
+    require("gruvbox").setup({
+      contrast = "soft", -- soft | medium | hard
+      transparent_mode = true,
+      dim_inactive = false,
     })
 
-    vim.cmd("colorscheme github_dark_dimmed")
+    vim.cmd.colorscheme("gruvbox")
   end,
 }
